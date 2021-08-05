49d141ce49cb5434efe7b05cf06d7dd578e11183
unknown <truongsontung@gmail.com>
2021-08-06 05:20:29 +0700

    bo sung bai toan thap hanoi

--- BasicAlgorithms.py
+++ BasicAlgorithms.py
@@ -2,7 +2,7 @@
 # Gợi ý:
 # Sử dụng đệ quy
 # Không sử dụng đệ quy
-def fibonacci_nonRecur(n):
+def fibonacci_nonRecur(n)->list:
     """
     ham tra ve list [] day so fibonacci f0,f1,f2,.....fn
     ham su dung vong lap for tinh va nap cac gia tri vao list []
@@ -21,7 +21,7 @@ def fibonacci_nonRecur(n):
         for i in range(2,n+1):      
             fib.append(fib[i-1]+fib[i-2])
         return fib
-def fibonacci_Recur(n):
+def fibonacci_Recur(n)->list:
     """
     ham tra ve list [] day so fibonacci f0,f1,f2,.....fn
     ham su dung de quy tinh va nap cac gia tri vao list []
@@ -40,9 +40,40 @@ def fibonacci_Recur(n):
     for i in range(0,n+1):
         fib.append(__run_Recur(i))
     return fib
-    return __run_Recur(2)
+
+def HaNoitower(n,a,b,c)->list:
+    """       a  -------->   b      -------->   c        
+           ///////           H                  H
+        ///////////          H                  H
+      //////////////         H                  H
+    //////////////////       H                  H
+    nếu có 1 đĩa :
+        a ------->b
+    nếu có n đĩa:
+        (n-1) ----------------------------->    c
+         a----------------> b
+                            b <-------------  (n-1)
+        plan = HaNoitower(3,1,2,3) with n= 3, col 1, col 2, col 3
+    """
+    __plan_sub=[]
+    def __movetower__(h,c1,c2,c3):
+        if h==1:  
+            __plan_sub.append([c1,">>",c2])
+        else:
+            __movetower__(h-1,c1,c3,c2)
+            __plan_sub.append([c1,">>",c2])
+            __movetower__(h-1,c3,c2,c1)
+    __movetower__(n,a,b,c)
+    return __plan_sub
+
 if __name__=="__main__":
-    # print(fibonacci_nonRecur.__doc__)
-    print(fibonacci_nonRecur(10))
-    # print(fibonacci_Recur.__doc__)
-    print(fibonacci_Recur(10))
+    # # print(fibonacci_nonRecur.__doc__)
+    # print(fibonacci_nonRecur(10))
+    # # print(fibonacci_Recur.__doc__)
+    # print(fibonacci_Recur(10))
+    plan =HaNoitower(4,1,2,3)
+    for i in plan:
+        print(i)
+
+
+
